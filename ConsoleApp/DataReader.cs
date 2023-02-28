d5ab3d74b9c56c10962f601a24733c28d0e0491c
srednimax <sredniamx@gmail.com>
2023-02-28 14:02:38 +0100

    invert if statements

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -84,24 +84,24 @@
                     // print all database's tables
                     foreach (var table in ImportedObjects)
                     {
-                        if (table.ParentType.ToUpper() == database.Type)
-                        {
-                            if (table.ParentName == database.Name)
-                            {
-                                Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+                        if (table.ParentType.ToUpper() != database.Type)
+                            continue;
 
-                                // print all table's columns
-                                foreach (var column in ImportedObjects)
-                                {
-                                    if (column.ParentType.ToUpper() == table.Type)
-                                    {
-                                        if (column.ParentName == table.Name)
-                                        {
-                                            Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
-                                        }
-                                    }
-                                }
-                            }
+                        if (table.ParentName != database.Name)
+                            continue;
+
+                        Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+
+                        // print all table's columns
+                        foreach (var column in ImportedObjects)
+                        {
+                            if (column.ParentType.ToUpper() != table.Type)
+                                continue;
+
+                            if (column.ParentName != table.Name)
+                                continue;
+
+                            Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
                         }
                     }
                 }
